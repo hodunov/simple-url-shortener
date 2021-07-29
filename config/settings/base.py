3d26aad44c11c7343af496c67fb9750df334e35a
Artem Hodunov <hodunov.art@gmail.com>
2021-07-29 22:02:36 +0300

    Add short_url to INSTALLED_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,6 +73,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "simple_url_shortener.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "short_url",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
