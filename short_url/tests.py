e33dbb424ba21ac0a2b031ee8b02fcc5f017a31d
Artem Hodunov <hodunov.art@gmail.com>
2021-07-29 22:21:55 +0300

    Add test: ShortenerTestCase test_create_short_url

--- short_url/tests.py
+++ short_url/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from short_url.models import Shortener
 
-# Create your tests here.
+
+class ShortenerTestCase(TestCase):
+    def test_create_short_url(self):
+        site_url = "https://example.com/"
+        Shortener.objects.create(full_url=site_url)
+        url_obj = Shortener.objects.first()
+        short_url = url_obj.short_url
+        full_url = url_obj.full_url
+        self.assertNotEqual(site_url, short_url)
+        self.assertEqual(site_url, full_url)
