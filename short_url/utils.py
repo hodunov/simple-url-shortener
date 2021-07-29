5d74976114b98d118740441bc243a6b96eaf2932
Artem Hodunov <hodunov.art@gmail.com>
2021-07-29 22:08:16 +0300

    Add utils for Shortener model: create_shortened_url func

--- short_url/utils.py
+++ short_url/utils.py
@@ -15,3 +15,15 @@ def create_random_code(chars=AVAIABLE_CHARS):
     Creates a random string with the predetermined size
     """
     return "".join([choice(chars) for _ in range(SIZE)])
+
+
+def create_shortened_url(model_instance):
+    random_code = create_random_code()
+    # Gets the model class
+    model_class = model_instance.__class__
+
+    if model_class.objects.filter(short_url=random_code).exists():
+        # Run the function again
+        return create_shortened_url(model_instance)
+
+    return random_code
